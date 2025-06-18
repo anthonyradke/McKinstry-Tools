dd00222d143fb2fdf0dd21571b5e23e3fa533948
anra7029 <anthony.radke@outlook.com>
2025-06-18 08:56:50 -0600

    file struc

--- archive/app_placeholder1.py
+++
@@ -1,5 +0,0 @@
-import streamlit as st
-
-def run():
-    st.title("📊 Placeholder Tool 1")
-    st.info("This tool will be developed soon!")
--- archive/app_placeholder2.py
+++
@@ -1,5 +0,0 @@
-import streamlit as st
-
-def run():
-    st.title("📁 Placeholder Tool 2")
-    st.info("This tool will be developed soon!")
